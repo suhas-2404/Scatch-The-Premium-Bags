f33fdc502d625a639e2762ac88b7cdaa5821e3eb
unknown <suhasrajeshmysore@gmail.com>
2025-01-21 15:09:56 +0530

    Added features for profile

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,23 @@
 const express=require('express');
 const router=express.Router();
+const userModel = require('../models/user-model');
 
 const {registerUser}=require('../controller/authController');
 const {userLogin}=require('../controller/authController');
 const {userLogout}=require('../controller/authController');
+const isLoggedIn = require('../middleware/isLoggedIn');
 
 router.get('/',(req,res)=>{
     res.send('Owner Router');
+});
+
+router.get('/profile', isLoggedIn, async (req, res) => {
+    try {
+        const user = await userModel.findById(req.user._id);  // Assuming `req.user` contains the logged-in user's information
+        res.render('profile', { user }); // Render the profile page and pass the user data
+    } catch (err) {
+        res.status(500).send('Error fetching user data: ' + err.message);
+    }
 });
 
 router.post('/register',registerUser);
